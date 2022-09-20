6bbef72de8182db53cf145251b858fd9c80b457a
William Saville <williamlmsaville@gmail.com>
2022-09-21 04:56:46 +0900

    Installing tailwind with postcss and extensions

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import dolphinImg from 'public/images/dolphin-orange.svg'
+import dolphinImg from '@/public/images/dolphin-orange.svg'
 
 export const Logo = () => (
   <Image src={dolphinImg} width={200} height={200} alt="dolphin logo" />
