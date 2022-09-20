09b6b96ede2eddb0582222257422397209d8b655
William Saville <williamlmsaville@gmail.com>
2022-09-21 01:24:06 +0900

    Moved public folder outside src to fix favicon issue

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import dolphinImg from 'public/dolphin-orange.svg'
+import dolphinImg from 'public/images/dolphin-orange.svg'
 
 export const Logo = () => (
-  <Image src={dolphinImg} width={200} height={200} />
+  <Image src={dolphinImg} width={200} height={200} alt="dolphin logo" />
 )
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,6 +3,8 @@ import { Home } from '../components/Home';
 
 export default function Workspace() {
   return (
-    <Home />
+    <>
+      <Home />
+    </>
   )
 }
